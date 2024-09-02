ea69d5516a604489c3fd5efa823a47c7cfc50f6f
Parth Mehendru <112183906+parthmehendru@users.noreply.github.com>
2024-09-02 21:57:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import reddit from './redditapi';
+const reddit = {
+    search: function(searchTerm, searchLimit, sortBy){
+        return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+        .then(res => res.json())
+        .then(data => data.data.children.map(element => element.data))
+        .catch(err => console.log(err));
+    }
+}
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
